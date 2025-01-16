4ca56f4cfaa96c9753701f108344cd37179b3d77
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-17 00:31:53 +0300

    Improved main.go

--- src/main.go
+++ src/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"regexp"
 	"strings"
 )
@@ -56,10 +60,6 @@ func CreateAnalyzedString(numOfString uint32, s *string) AnalyzedString {
 func getMap(slice []rune) map[rune]uint {
 	ret := map[rune]uint{}
 	for _, symbol := range slice {
-		/* _, ok := ret[symbol]
-		if ok {
-			ret[symbol] = 0
-		} */
 		ret[symbol]++
 	}
 	return ret
@@ -69,7 +69,90 @@ func checkYo(s *string) bool {
 	return strings.ContainsAny(*s, "ёЁ")
 }
 
+func loadFromFile(fpath *string) []string {
+	file, err := os.Open(*fpath)
+	if err != nil {
+		l := log.New(os.Stderr, "", 0)
+		l.Println(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	var ret []string
+	for {
+		str, err := reader.ReadString('\n')
+		if err == io.EOF {
+			return ret
+		}
+		if err != nil {
+			l := log.New(os.Stderr, "", 0)
+			l.Println(err)
+		}
+		ret = append(ret, str)
+	}
+}
+
+type ArgsParser struct {
+	pseudonyms     map[string]string
+	possibleArgs   map[string]uint
+	functionMapper map[string]func([]string)
+}
+
+func ConstructParser(entities ...ArgEntity) ArgsParser {
+
+}
+
+type ArgEntity struct {
+	ArgName            string
+	AssociatedFunction func([]string)
+	Pseudonyms         []string
+}
+
+func ConstructEntity(function func([]string), name string, pseudonyms ...string) ArgEntity {
+	return ArgEntity{name, function, pseudonyms}
+}
+
+func parseArgs(args []string) map[string][]string {
+	result := make(map[string][]string)
+	var currentKey string
+
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "--") {
+			currentKey = strings.TrimLeft(arg, "--")
+			if currentKey == "" {
+				panic("Invalid argument key")
+			}
+			_, exists := result[currentKey]
+			if exists {
+				panic(fmt.Sprintf("Duplicate argument key: %s", currentKey))
+			}
+
+			result[currentKey] = []string{}
+			continue
+		}
+		if strings.HasPrefix(arg, "-") {
+			currentKey = strings.TrimLeft(arg, "-")
+			if currentKey == "" {
+				panic("Invalid argument key")
+			}
+
+			if _, exists := result[currentKey]; exists {
+				panic(fmt.Sprintf("Duplicate argument key: %s", currentKey))
+			}
+
+			result[currentKey] = []string{}
+			continue
+		}
+		if currentKey == "" {
+			panic(fmt.Sprintf("Value provided without a key: %s", arg))
+		}
+		result[currentKey] = append(result[currentKey], arg)
+	}
+
+	return result
+}
+
 func main() {
+	args := os.Args
 	message := "Hello world"
 	fmt.Println(message)
 }
