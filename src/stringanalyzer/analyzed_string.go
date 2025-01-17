91231d3cabf65debfd5747c3d9899299bd305c1f
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-18 00:14:20 +0300

    Add combination map to string analyzer

--- src/stringanalyzer/analyzed_string.go
+++ src/stringanalyzer/analyzed_string.go
@@ -1,10 +1,11 @@
 package stringanalyzer
 
 type AnalyzedString struct {
-	innerString  *string
-	sizeOfString int
-	symbolMap    map[rune]uint
-	flagMap      map[string]bool
+	innerString    *string
+	sizeOfString   int
+	symbolMap      map[rune]uint
+	combinationMap map[[2]rune]uint
+	flagMap        map[string]bool
 }
 
 func (as AnalyzedString) GetString() *string {
@@ -34,14 +35,22 @@ func constructAnalyzedString(
 	for _, flag := range flags {
 		flagMap[flag.key] = flag.b
 	}
-	symbolMap := constructSymMap([]rune(*str))
-	return AnalyzedString{str, len(*str), symbolMap, flagMap}
+	symbolMap, combinationMap := constructSymMap([]rune(*str))
+	return AnalyzedString{str, len(*str), symbolMap, combinationMap, flagMap}
 }
 
-func constructSymMap(slice []rune) map[rune]uint {
-	ret := map[rune]uint{}
-	for _, symbol := range slice {
-		ret[symbol]++
+func constructSymMap(slice []rune) (map[rune]uint, map[[2]rune]uint) {
+	retOne := map[rune]uint{}
+	retTwo := map[[2]rune]uint{}
+	var previousSymbol rune
+	for i, symbol := range slice {
+		retOne[symbol]++
+		if i == 0 {
+			previousSymbol = symbol
+			continue
+		}
+		retTwo[[2]rune{previousSymbol, symbol}]++
+		previousSymbol = symbol
 	}
-	return ret
+	return retOne, retTwo
 }
