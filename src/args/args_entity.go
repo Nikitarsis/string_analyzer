fced1dd70a9edd245539e3c865fe6a740c23858c
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-17 12:12:11 +0300

    Changed to variadic

--- src/args/args_entity.go
+++ src/args/args_entity.go
@@ -2,15 +2,15 @@ package args
 
 type argEntity struct {
 	ArgName            string
-	AssociatedFunction func([]string)
+	AssociatedFunction func(...string)
 	Pseudonyms         []string
 	NargsChecker       func(uint) bool
 }
 
-func constructEntityChecker(function func([]string), name string, nargsChecker func(uint) bool, pseudonyms ...string) argEntity {
+func constructEntityChecker(function func(...string), name string, nargsChecker func(uint) bool, pseudonyms ...string) argEntity {
 	return argEntity{name, function, pseudonyms, nargsChecker}
 }
 
-func constructEntity(function func([]string), name string, nargs uint, pseudonyms ...string) argEntity {
+func constructEntity(function func(...string), name string, nargs uint, pseudonyms ...string) argEntity {
 	return constructEntityChecker(function, name, func(x uint) bool { return x == nargs }, pseudonyms...)
 }
