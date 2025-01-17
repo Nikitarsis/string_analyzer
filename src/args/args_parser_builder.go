508cb4d4f584dd40a44afc9c86c85171e697d3b0
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-17 13:47:42 +0300

    Finished arguments parser builder

---
+++ src/args/args_parser_builder.go
@@ -0,0 +1,89 @@
+package args
+
+import (
+	"slices"
+)
+
+type ArgsParserBuilder struct {
+	entities []argEntity
+}
+
+func NewParserBuilder() ArgsParserBuilder {
+	return ArgsParserBuilder{}
+}
+
+func (apb *ArgsParserBuilder) AddElement(
+	function func(...string),
+	checker func(uint) bool,
+	name string,
+	pseudonyms ...string) *ArgsParserBuilder {
+
+	add := constructEntityChecker(function, name, checker, pseudonyms...)
+	apb.entities = append(apb.entities, add)
+	return apb
+}
+
+func (apb *ArgsParserBuilder) AddElementSlice(
+	function func(...string),
+	possibleArguments []uint,
+	name string,
+	pseudonyms ...string) *ArgsParserBuilder {
+
+	checker := func(x uint) bool {
+		return slices.Contains(possibleArguments, x)
+	}
+	return apb.AddElement(function, checker, name, pseudonyms...)
+}
+
+func (apb *ArgsParserBuilder) AddElimentSingle(
+	function func(...string),
+	argument uint,
+	name string,
+	pseudonyms ...string) *ArgsParserBuilder {
+
+	checker := func(x uint) bool {
+		return argument == x
+	}
+	return apb.AddElement(function, checker, name, pseudonyms...)
+}
+
+func (apb *ArgsParserBuilder) AddElementAtLeast(
+	function func(...string),
+	minNumArguments uint,
+	name string,
+	pseudonyms ...string) *ArgsParserBuilder {
+
+	checker := func(x uint) bool {
+		return x >= minNumArguments
+	}
+	return apb.AddElement(function, checker, name, pseudonyms...)
+}
+
+func (apb *ArgsParserBuilder) AddElementAtMost(
+	function func(...string),
+	maxNumArguments uint,
+	name string,
+	pseudonyms ...string) *ArgsParserBuilder {
+
+	checker := func(x uint) bool {
+		return x <= maxNumArguments
+	}
+	return apb.AddElement(function, checker, name, pseudonyms...)
+}
+
+func (apb *ArgsParserBuilder) AddElementBetween(
+	function func(...string),
+	minNumArguments uint,
+	maxNumArguments uint,
+	name string,
+	pseudonyms ...string) *ArgsParserBuilder {
+
+	checker := func(x uint) bool {
+		return x <= maxNumArguments && x >= minNumArguments
+	}
+	return apb.AddElement(function, checker, name, pseudonyms...)
+}
+
+func (apb ArgsParserBuilder) Construct() ArgsParser {
+	return constructParser(apb.entities...)
+}
