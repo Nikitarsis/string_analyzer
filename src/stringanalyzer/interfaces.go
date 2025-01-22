b2201395c925556cac90e2b21dcb50bf2517d5da
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-22 14:55:06 +0300

    Add interfaces

---
+++ src/stringanalyzer/interfaces.go
@@ -0,0 +1,11 @@
+package stringanalyzer
+
+type ICanGetJSON interface {
+	GetJson() ([]byte, error)
+}
+
+type IAnalyzedString interface {
+	ICanGetJSON
+	GetSize() int
+	GetSymbolMap() map[rune]uint
+}
