6c37a85edaa8925eedc776c2bb903ffb4db47f42
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-22 15:00:57 +0300

    Changed main.go

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,8 @@ import (
 	"args"
 	"fmt"
 	"os"
+	"regexp"
+	"stringanalyzer"
 )
 
 //CLASS := regexp.MustCompile(`[ѢѣІіѲѳѴѵ]|([ВКСфкцнгшщзхфвпрлджчсмтб]ъ[ ,.;:?!\-"'])`)
@@ -12,15 +14,35 @@ import (
 
 func getParser() *args.ArgsParser {
 	builder := args.NewParserBuilder()
-	builder.AddElementAtLeast(func(s ...string) {}, 1, "input", true, "i")
-	builder.AddElementAtLeast(func(s ...string) {}, 1, "output", true, "o")
-	builder.AddElementAtMost(func(s ...string) {}, 0, "pipeline", false, "p")
+	builder.AddElementAtLeast(func(s ...string) {}, 1, "inputFile", false, "i")
+	builder.AddElementAtLeast(func(s ...string) {}, 1, "outputFile", false, "o")
+	builder.AddElementAtMost(func(s ...string) {}, 0, "saveStrings", false, "s")
+	builder.AddElementAtMost(func(s ...string) {}, 0, "countCombinations", false, "c")
+	builder.AddElementAtMost(func(s ...string) {}, 0, "nopipeline", false, "n")
 	builder.AddElementAtMost(func(s ...string) {}, 0, "debug", false, "d")
 	ret, err := builder.Construct()
 	if err != nil {
 		panic(err)
 	}
 	return ret
+}
+
+func getStringAnalyzer(saveStr bool, countComb bool) *stringanalyzer.StringAnalyzer {
+	CLASS := regexp.MustCompile(`[ѢѣІіѲѳѴѵ]|([ВКСфкцнгшщзхфвпрлджчсмтб]ъ[ ,.;:?!\-"'])`)
+	REFORM := regexp.MustCompile(`([иИ][яеёоыеиюэ])|([ВКСфкцнгшщзхфвпрлджчсмтб][ ,.;:?!\-"'])`)
+	TRASH := regexp.MustCompile(`.{,5}`)
+
+	builder := stringanalyzer.CreateSABuilder()
+	if countComb {
+		builder.SaveCombinations()
+	}
+	if saveStr {
+		builder.SaveOriginalString()
+	}
+	builder.AddChecker("isClassical", func(s *string) bool { return CLASS.MatchString(*s) })
+	builder.AddChecker("isReformed", func(s *string) bool { return REFORM.MatchString(*s) })
+	builder.AddChecker("isTrash", func(s *string) bool { return TRASH.MatchString(*s) })
+	return builder.Construct()
 }
 
 func main() {
