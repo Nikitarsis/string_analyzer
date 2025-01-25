4029e8d59fa4f4ea8756560eca5ba844ac0c9b7e
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-25 16:33:56 +0300

    Change Parser

--- src/args/args.go
+++ src/args/args.go
@@ -13,46 +13,43 @@ func DivideArgs(args ...string) (map[string][]string, error) {
 
 	//Checks every argument
 	for _, arg := range args {
+		var check bool
 		//If argument has duble minus, argument is count as single key
-		if strings.HasPrefix(arg, "--") {
-			currentKey = strings.TrimLeft(arg, "--")
-			//Checking incorrect keys
+		if currentKey, check = strings.CutPrefix(arg, "--"); check {
+			//Checking incorrect key
 			if currentKey == "" {
-				return nil, errors.New("Invalid argument key")
+				return nil, errors.New("invalid argument key")
 			}
 			_, exists := result[currentKey]
 			if exists {
-				return nil, errors.New(fmt.Sprintf("Duplicate argument key: %s", currentKey))
+				return nil, fmt.Errorf("duplicate argument key: %s", currentKey)
 			}
-
 			result[currentKey] = []string{}
 			continue
 		}
 		//If argument has single minus, every letter interpretes as key
-		if strings.HasPrefix(arg, "-") {
-			keys := strings.Split(strings.TrimLeft(arg, "-"), "")
+		if currentKey, check = strings.CutPrefix(arg, "-"); check {
+			keys := strings.Split(currentKey, "")
 			if len(keys) == 0 {
-				return nil, errors.New("Invalid argument key")
+				return nil, errors.New("invalid argument key")
 			}
 			for _, currentKey = range keys {
 				//Checking incorrect keys
 				if currentKey == "" {
-					return nil, errors.New("Invalid argument key")
+					return nil, errors.New("invalid argument key")
 				}
 
 				if _, exists := result[currentKey]; exists {
-					return nil, errors.New(fmt.Sprintf("Duplicate argument key: %s", currentKey))
+					return nil, fmt.Errorf("duplicate argument key: %s", currentKey)
 				}
-
 				result[currentKey] = []string{}
 			}
 			continue
 		}
 		if currentKey == "" {
-			return nil, errors.New(fmt.Sprintf("Value provided without a key: %s", arg))
+			return nil, fmt.Errorf("value provided without a key: %s", arg)
 		}
 		result[currentKey] = append(result[currentKey], arg)
 	}
-
 	return result, nil
 }
